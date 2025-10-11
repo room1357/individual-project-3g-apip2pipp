bf3c415f0b199166f01fe236fb0fed7465021280
apip2pipp <afifkhosyidzaki@gmail.com>
2025-10-11 07:50:01 +0700

    .feat : version of app and make route for screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,32 @@
 import 'package:flutter/material.dart';
-import 'screens/login_screen.dart';
+import 'package:provider/provider.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
-void main() {
-  runApp(const MyApp());
+import 'services/expense_service.dart';
+import 'services/storage_service.dart';
+
+// screens
+import 'screens/login_screen.dart';
+import 'screens/home_screen.dart';
+import 'screens/expense_list_screen.dart';
+import 'screens/category_screen.dart';
+import 'screens/statistics_screen.dart';
+import 'screens/profile_screen.dart';
+import 'screens/settings_screen.dart';
+import 'screens/about_screen.dart';
+import 'screens/looping_demo_screen.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await initializeDateFormatting('id_ID', null);
+
+  runApp(
+    ChangeNotifierProvider(
+      create:
+          (_) => ExpenseService(StorageService())..init(fallbackUser: 'user-1'),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -15,8 +39,22 @@ class MyApp extends StatelessWidget {
       title: 'Aplikasi Pengeluaran',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
       ),
-      home: const LoginScreen(),
+
+      // pakai named routes
+      initialRoute: '/login',
+      routes: {
+        '/login': (_) => const LoginScreen(),
+        '/home': (_) => const HomeScreen(),
+        '/expenses': (_) => const ExpenseListScreen(), // ⬅️ ditambahkan
+        '/categories': (_) => const CategoryScreen(),
+        '/stats': (_) => const StatisticsScreen(),
+        '/profile': (_) => const ProfileScreen(),
+        '/settings': (_) => const SettingsScreen(),
+        '/about': (_) => const AboutScreen(),
+        '/loops ': (_) => const LoopingDemoScreen(),
+      },
     );
   }
 }

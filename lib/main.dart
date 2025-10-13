612c566749428246c12b06849c2739415512de56
apip2pipp <afifkhosyidzaki@gmail.com>
2025-10-13 16:23:58 +0700

    .feat: add authentication and splash screen, update routing

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
+// lib/main.dart
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:intl/date_symbol_data_local.dart';
 
 import 'services/expense_service.dart';
 import 'services/storage_service.dart';
+import 'services/auth_service.dart';
 
 // screens
 import 'screens/login_screen.dart';
 import 'screens/home_screen.dart';
+import 'screens/splash_screen.dart';
 import 'screens/expense_list_screen.dart';
 import 'screens/category_screen.dart';
 import 'screens/statistics_screen.dart';
@@ -21,9 +24,18 @@ Future<void> main() async {
   await initializeDateFormatting('id_ID', null);
 
   runApp(
-    ChangeNotifierProvider(
-      create:
-          (_) => ExpenseService(StorageService())..init(fallbackUser: 'user-1'),
+    MultiProvider(
+      providers: [
+        // ⬇️ PROVIDER UNTUK AUTH (wajib ada agar context.read<AuthService>() tidak error)
+        ChangeNotifierProvider(create: (_) => AuthService()..loadSession()),
+        // ⬇️ PROVIDER LAMA UNTUK EXPENSE
+        ChangeNotifierProvider(
+          create:
+              (_) =>
+                  ExpenseService(StorageService())
+                    ..init(fallbackUser: 'user-1'),
+        ),
+      ],
       child: const MyApp(),
     ),
   );
@@ -36,24 +48,25 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Aplikasi Pengeluaran',
+      title: 'SakuRapi',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
 
       // pakai named routes
-      initialRoute: '/login',
+      initialRoute: '/splash',
       routes: {
+        '/splash': (_) => const SplashScreen(),
         '/login': (_) => const LoginScreen(),
         '/home': (_) => const HomeScreen(),
-        '/expenses': (_) => const ExpenseListScreen(), // ⬅️ ditambahkan
+        '/expenses': (_) => const ExpenseListScreen(),
         '/categories': (_) => const CategoryScreen(),
         '/stats': (_) => const StatisticsScreen(),
         '/profile': (_) => const ProfileScreen(),
         '/settings': (_) => const SettingsScreen(),
         '/about': (_) => const AboutScreen(),
-        '/loops ': (_) => const LoopingDemoScreen(),
+        '/loops': (_) => const LoopingDemoScreen(), // <- tanpa spasi
       },
     );
   }

452eaf03d24c1a755189d25af958202cd27b7ad2
apip2pipp <afifkhosyidzaki@gmail.com>
2025-10-20 22:43:02 +0700

    fix: correct initial route from '/splashs' to '/splash' in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -93,7 +93,7 @@ class MyApp extends StatelessWidget {
       ),
 
       // pakai named routes
-      initialRoute: '/splashs',
+      initialRoute: '/splash',
       routes: {
         '/splash': (_) => const SplashScreen(),
         '/onboarding': (_) => const OnboardingScreen(),
